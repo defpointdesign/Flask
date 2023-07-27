2f380b84035c9306deceec7f38cb1c8f7028b732
defpoint <defpointdesign@gmail.com>
2023-07-27 12:20:13 +0200

    function url_for and variable URL-addresses

--- flsite.py
+++ flsite.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template
-import json
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
 
+app = Flask(__name__)
 
 menu = ['Instalation', 'First Application', 'Feedback' ]
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template ('index.html', menu=menu)
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html', title='About site', menu=menu)
 
+@app.route("/profile/<username>")
+def profile(username):
+    return f"User:{username}"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('about'))
+    print(url_for('profile', username="defpointdesign"))
+
+# # web server
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 
