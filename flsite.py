fb321df3b68e63c49979c8d2354925aab36cf6b6
defpoint <defpointdesign@gmail.com>
2023-07-27 14:32:10 +0200

    connecting file to HTM; work with forms

--- flsite.py
+++ flsite.py
@@ -1,29 +1,30 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ['Instalation', 'First Application', 'Feedback' ]
+menu =[{"name": "Installation", "url": "install-flask"},
+       {"name": "First Application", "url": "first-app"},
+       {"name": "Feedback", "url": "contact"}]
+
+
+
 @app.route("/")
 def index():
-    print(url_for('index'))
     return render_template ('index.html', menu=menu)
 
 @app.route("/about")
 def about():
-    print(url_for('about'))
     return render_template('about.html', title='About site', menu=menu)
 
-@app.route("/profile/<username>")
-def profile(username):
-    return f"User:{username}"
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    # checking request POST
+    if request.method == 'POST':
+        print(request.form['username'])
+    return render_template('contact.html', title='Feedback', menu=menu)
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('about'))
-    print(url_for('profile', username="defpointdesign"))
-
-# # web server
-# if __name__ == "__main__":
-#     app.run(debug=True)
+# web server
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
